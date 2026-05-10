1149895278601aa07e6fa97b5e331de6811b3657
Cepri Anto <cepriato131@gmail.com>
2026-05-10 19:39:55 +0700

    Built web
    Branch: main
    Commit: ac5e8564fbdd7cc1f6ec4ed510c4b7f946aba1e6
    
    package:peanut 6.0.1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3712581254"
+    serviceWorkerVersion: "390457652"
   }
 });
